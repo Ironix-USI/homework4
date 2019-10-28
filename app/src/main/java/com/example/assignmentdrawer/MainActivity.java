33c6f1c0451b3a17d542b01144be9bfba26c9f37
Reinard Lazuardi Kuwandy <reinardlazuardikuwandy@Reinards-MacBook-Pro.local>
2019-10-28 14:02:29 +0100

    change floating button icon

--- app/src/main/java/com/example/assignmentdrawer/MainActivity.java
+++ app/src/main/java/com/example/assignmentdrawer/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, getString(R.string.pick_our_menu), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
